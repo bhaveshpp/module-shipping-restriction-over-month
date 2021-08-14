a25db3c775020fa11a4d311c2e81af3ce21e2f2e
bhaveshpp <bhaveshpp11@gmail.com>
2021-08-14 19:31:01 +0530

    reg module

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Bhaveshpp 
+ *
+ * This code is developed by Bhavesh Prajapati
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @authors https://github.com/bhaveshpp
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Bhaveshpp_ShippingRestrictionOverMonth',
+    __DIR__
+);
